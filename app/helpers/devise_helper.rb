4441b14f9a07bfcdbea5754aba08b5036e447e32
Jessseee <jessewiebe2000@gmail.com>
2018-01-17 11:50:33 +0100

    Added profile edit page

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -4,7 +4,7 @@ module DeviseHelper
 
     errors = resource.errors.full_messages.map do |msg|
       html = <<-HTML
-      <script type="text/javascript">Materialize.toast("#{msg}", 8000, 'devise-error');</script>
+      <script type="text/javascript">Materialize.toast("#{msg}", 6000, 'devise-error');</script>
       HTML
     end
     errors.join.html_safe
