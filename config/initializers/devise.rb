03dcb24bb3f4d3b0f5b066f42a502367edfb03a9
Jessseee <jessewiebe2000@gmail.com>
2018-01-18 16:15:11 +0100

    Added username to user model

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -225,7 +225,7 @@ config.secret_key = '5d1f6b18be28f1c96422c4d75ed5dd1a00356c56601e441df5ad9c9b81e
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
   # "users/sessions/new". It's turned off by default because it's slower if you
   # are using only default views.
-  # config.scoped_views = false
+config.scoped_views = true
 
   # Configure the default scope given to Warden. By default it's the first
   # devise role declared in your routes (usually :user).
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_for :user, controllers: {
+  # confirmations: 'users/confirmations',
+  passwords: 'users/passwords',
+  registrations: 'users/registrations',
+  sessions: 'users/sessions',
+  # unlocks: 'users/unlocks',
+}, skip: [:sessions]
+
+  as :user do
+    get 'login' => 'users/sessions#new', as: :new_user_session
+    post 'login' => 'users/sessions#create', as: :user_session
+    delete 'logout' => 'users/sessions#destroy', as: :destroy_user_session
+    get 'register' => 'users/registrations#new'
+  end
 
   root 'welcome#index'
 
